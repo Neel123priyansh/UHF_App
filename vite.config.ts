20059676944fbde6cf12e7c529f420bddd7ce814
Neel123priyansh <neelpriyansh@gmail.com>
2025-11-06 01:09:41 +0530

    Remove

--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,17 @@ import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), VitePWA({
     registerType: 'autoUpdate',
       includeAssets: ['favicon.svg', 'robots.txt', 'apple-touch-icon.png'],
       manifest: {
         name: 'My React PWA',
-        short_name: 'Student Verification',
+        short_name: 'MyPWA',
         description: 'A sample React PWA',
         start_url: '/',
         display: 'standalone',
+        background_color: '#ffffff',
         theme_color: '#0f172a',
         icons: [
           { src: '/public/assets/icons/Logo_192.png', sizes: '192x192', type: 'image/png' },
