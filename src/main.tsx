0a8d62b8574123c10d2a90e2a46fa7320e90ae31
Neel123priyansh <neelpriyansh@gmail.com>
2025-11-06 01:19:26 +0530

    Remove

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import App from './App.tsx'
 import './swClient' // after ReactDOM.createRoot(...)
 
 if ('serviceWorker' in navigator && import.meta.env.PROD) {
-  navigator.serviceWorker.register('/sw.js').catch(console.error);
+  navigator.serviceWorker.register('/sw.js').then(reg => console.log('Service Worker registered:', reg.scope)).catch(err => console.error('SW registration failed:', err))
 }
 
 createRoot(document.getElementById('root')!).render(
